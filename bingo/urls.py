2a350fccd31a02b8b3d1b0fa22021af96470fb96
naitsa <naitsabot@gmail.com>
2024-07-13 13:31:19 +0200

    comments

--- bingo/urls.py
+++ bingo/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     # ex: /bingo/number - Number Detail Page
     #path("<int:number>/", views.number, name="number"),
+    # ex: /bingo/api/data - API Data - GET all picked numbers - POST pick a number
     path("api/data/", views.bingo_numbers_data, name='bingo_numbers_data'),
+    # ex: /bingo/api/data/$number - API Data - GET number detail/object
     path("api/data/<int:number>/", views.bingo_object, name='bingo_object'),
 ]
