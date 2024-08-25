7feb36db2606ffcc80e042ce6b27f113ab54aed2
naitsa <naitsabot@gmail.com>
2024-08-25 21:34:56 +0200

    fully #2 #3 partial #4

--- bingo/Static/bingo/bingo.js
+++ bingo/Static/bingo/bingo.js
@@ -1,7 +1,7 @@
 "use strict";
 //hello
 let drawnNumbers = [];
-let display, startTime, endTime, duration, interval, finalNumber, imageURL;
+let display, startTime, endTime, duration = 4500, interval, finalNumber, imageURL;
 const lowerbound = 1;
 const upperbound = 90;
 let obj;
@@ -47,7 +47,6 @@ async function postData(data, csrftoken) {
 
 async function startCycling() {
     display = document.querySelector(".image-container .image-text");
-    duration = 4444; // miliseconds
     startTime = Date.now();
     endTime = startTime + duration;
     
@@ -87,16 +86,93 @@ function updateNumber() {
     let currentTime = Date.now();
     let elapsedTime = currentTime - startTime;
     let remainingTime = endTime - currentTime;
-    let progress = elapsedTime / duration;
+    let progress = (elapsedTime / duration);
+
+    //disabling button
+    button.disabled = true;
 
     if (remainingTime <= 0) {
+        //set final number
         display.innerText = finalNumber;
+        //enabling button
+        button.disabled = false; 
+
         clearTimeout(interval);
         updateImage();
         updateBall();
     } else {
         display.innerText = getRandomNumber();
-        let intervalTime = Math.pow(progress, 2) * 100; // slows down over time
+        
+        let easedProgress;
+        switch (easing_input.value) {
+            case "linear":
+                easedProgress = easeLinear(progress);
+                break;
+            case "easeOutSine":
+                easedProgress = easeOutSine(progress);
+                break;
+            case "easeOutCubic":
+                easedProgress = easeOutCubic(progress);
+                break;
+            case "easeOutQuint":
+                easedProgress = easeOutQuint(progress);
+                break;
+            case "easeOutCirc":
+                easedProgress = easeOutCirc(progress);
+                break;
+            case "easeOutElastic":
+                easedProgress = easeOutElastic(progress);
+                break;
+            case "easeOutQuad":
+                easedProgress = easeOutQuad(progress);
+                break;
+            case "easeOutQuart":
+                easedProgress = easeOutQuart(progress);
+                break;
+            case "easeOutExpo":
+                easedProgress = easeOutExpo(progress);
+                break;
+            case "easeOutBack":
+                easedProgress = easeOutBack(progress);
+                break;
+            case "easeOutBounce":
+                easedProgress = easeOutBounce(progress);
+                break;
+            case "easeInOutSine":
+                easedProgress = easeInOutSine(progress);
+                break;
+            case "easeInOutCubic":
+                easedProgress = easeInOutCubic(progress);
+                break;
+            case "easeInOutQuint":
+                easedProgress = easeInOutQuint(progress);
+                break;
+            case "easeInOutCirc":
+                easedProgress = easeInOutCirc(progress);
+                break;
+            case "easeInOutElastic":
+                easedProgress = easeInOutElastic(progress);
+                break;
+            case "easeInOutQuad":
+                easedProgress = easeInOutQuad(progress);
+                break;
+            case "easeInOutQuart":
+                easedProgress = easeInOutQuart(progress);
+                break;
+            case "easeInOutExpo":
+                easedProgress = easeInOutExpo(progress);
+                break;
+            case "easeInOutBack":
+                easedProgress = easeInOutBack(progress);
+                break;
+            case "easeInOutBounce":
+                easedProgress = easeInOutBounce(progress);
+                break;
+            default:
+                easedProgress = easeLinear(progress);
+        }
+
+        let intervalTime = Math.pow(easedProgress, 2) * 100; // slows down over time
         interval = setTimeout(updateNumber, intervalTime);
     }
 }
@@ -145,3 +221,145 @@ wipe_form.addEventListener("submit", async function(event) {
         console.log("No wipe"); 
     }    
 });
+
+const time_input = document.getElementById("settime_id");
+time_input.addEventListener("change", function(event) {
+    duration = event.target.value * 1000;
+    console.log(duration);
+});
+
+const easing_input = document.getElementById("seteasing_id");
+easing_input.addEventListener("change", function(event) {
+    console.log(event.target.value);
+});
+
+
+
+
+// Easing functions
+
+function easeLinear(x) {
+    return x;
+}
+
+function easeOutSine(x) {
+    return Math.sin((x * Math.PI) / 2);
+}
+
+function easeOutCubic(x) {
+    return 1 - Math.pow(1 - x, 3);
+}
+
+function easeOutQuint(x) {
+    return 1 - Math.pow(1 - x, 5);
+}
+
+function easeOutCirc(x) {
+    return Math.sqrt(1 - Math.pow(x - 1, 2));
+}
+
+function easeOutElastic(x) {
+    const c4 = (2 * Math.PI) / 3;
+
+    return x === 0
+        ? 0
+        : x === 1
+        ? 1
+        : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
+}
+
+function easeOutQuad(x) {
+    return x * x;
+}
+
+function easeOutQuart(x) {
+    return 1 - Math.pow(1 - x, 4);
+}
+
+function easeOutExpo(x) {
+    return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+}
+
+function easeOutBack(x) {
+    const c1 = 1.70158;
+    const c3 = c1 + 1;
+
+    return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
+}
+
+function easeOutBounce(x) {
+    const n1 = 7.5625;
+    const d1 = 2.75;
+
+    if (x < 1 / d1) {
+        return n1 * x * x;
+    } else if (x < 2 / d1) {
+        return n1 * (x -= 1.5 / d1) * x + 0.75;
+    } else if (x < 2.5 / d1) {
+        return n1 * (x -= 2.25 / d1) * x + 0.9375;
+    } else {
+        return n1 * (x -= 2.625 / d1) * x + 0.984375;
+    }
+}
+
+function easeInOutSine(x) {
+    return -(Math.cos(Math.PI * x) - 1) / 2;
+}
+
+function easeInOutCubic(x) {
+    return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+}
+
+function easeInOutQuint(x) {
+    return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
+}
+
+function easeInOutCirc(x) {
+    return x < 0.5
+        ? (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2
+        : (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
+}
+
+function easeInOutElastic(x) {
+    const c5 = (2 * Math.PI) / 4.5;
+
+    return x === 0
+        ? 0
+        : x === 1
+        ? 1
+        : x < 0.5
+        ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
+        : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
+}
+
+function easeInOutQuad(x) {
+    return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
+}
+
+function easeInOutQuart(x) {
+    return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
+}
+
+function easeInOutExpo(x) {
+    return x === 0
+        ? 0
+        : x === 1
+        ? 1
+        : x < 0.5 ? Math.pow(2, 20 * x - 10) / 2
+        : (2 - Math.pow(2, -20 * x + 10)) / 2;
+}
+
+function easeInOutBack(x) {
+    const c1 = 1.70158;
+    const c2 = c1 * 1.525;
+
+    return x < 0.5
+        ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+        : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+}
+
+function easeInOutBounce(x) {
+    return x < 0.5
+        ? (1 - easeOutBounce(1 - 2 * x)) / 2
+        : (1 + easeOutBounce(2 * x - 1)) / 2;
+}
