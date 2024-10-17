84887bf673246a65294f42b6403be81f2998e3c7
naitsa <naitsabot@gmail.com>
2024-10-17 15:45:31 +0200

    correction

--- BitBingo/settings.py
+++ BitBingo/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_FINDERS = {
     'django.contrib.staticfiles.finders.FileSystemFinder',
